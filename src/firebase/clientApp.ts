9b5952f263ba3d1d650a33703bab5a81238e5ad9
Yatin Gambhir <gat2121215@sicsr.ac.in>
2024-04-25 11:04:27 +0530

    add ignore file

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -5,7 +5,7 @@ import { getStorage } from "firebase/storage";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBqe95nCUomWkU9PnZT_OlV_NCAZnTmyz0",
+  apiKey: "${process.env.FIREBASE_API_KEY}",
   authDomain: "reddit-clone-r.firebaseapp.com",
   projectId: "reddit-clone-r",
   storageBucket: "reddit-clone-r.appspot.com",
